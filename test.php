9218f05d99c349e5f0693c2a15daf39b35a5dd08
TheForgem4ster <maxim.thudercat@gmail.com>
2021-11-12 13:11:24 +0200

    Version_1

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Автор: ст.гр. КИУКИ-19-5 Жук Максим
+    </h1>
+Калькулятор<br>
+    <?php
+    
+    ?>
+    
+</body>
+</html>
